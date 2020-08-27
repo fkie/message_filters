3773e62286c714c34e65fbea35da77fdfe73b914
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2020-08-27 12:53:10 +0200

    Add test for subscriber callbacks

--- fkie_message_filters/rostest/test_publish_subscribe.cpp
+++ fkie_message_filters/rostest/test_publish_subscribe.cpp
@@ -28,6 +28,15 @@
 
 namespace mf = fkie_message_filters;
 
+namespace {
+    void process_pending_events()
+    {
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(50ms); // Apparently, we need to allow for some time to complete the message round trip
+        ros::spinOnce();
+    }
+}
+
 TEST(fkie_message_filters_ros_integration, Publisher)
 {
     std::size_t received_msgs = 0;
@@ -38,14 +47,42 @@ TEST(fkie_message_filters_ros_integration, Publisher)
     src.connect_to_sink(pub);
     ASSERT_EQ(0, sub.getNumPublishers());
     pub.advertise(nh, "publisher_test", 1);
-    ros::spinOnce();
+    process_pending_events();
     ASSERT_EQ(1, sub.getNumPublishers());
     ASSERT_EQ(0, received_msgs);
     src(std_msgs::Empty());
-    ros::spinOnce();
-    usleep(100000);
-    ros::spinOnce();
+    process_pending_events();
     ASSERT_EQ(1, received_msgs);
+}
+
+TEST(fkie_message_filters_ros_integration, PublisherWithSubscriberCB)
+{
+    std::size_t received_msgs = 0, connected_cbs = 0, disconnected_cbs = 0;
+    ros::NodeHandle nh("~");
+    mf::UserSource<std_msgs::Empty> src;
+    mf::Publisher<std_msgs::Empty, mf::RosMessage> pub;
+    src.connect_to_sink(pub);
+    pub.advertise(nh, "publisher_test", 1,
+        [&connected_cbs](const ros::SingleSubscriberPublisher&) { ++connected_cbs; },
+        [&disconnected_cbs](const ros::SingleSubscriberPublisher&) { ++disconnected_cbs; }
+    );
+    process_pending_events();
+    ASSERT_EQ(false, pub.is_active());
+    ASSERT_EQ(0, connected_cbs);
+    ASSERT_EQ(0, disconnected_cbs);
+    ros::Subscriber sub = nh.subscribe<std_msgs::Empty>("publisher_test", 1, [&received_msgs](const std_msgs::EmptyConstPtr&) { ++received_msgs; });
+    process_pending_events();
+    ASSERT_EQ(1, sub.getNumPublishers());
+    ASSERT_EQ(0, received_msgs);
+    ASSERT_EQ(1, connected_cbs);
+    ASSERT_EQ(0, disconnected_cbs);
+    src(std_msgs::Empty());
+    process_pending_events();
+    ASSERT_EQ(1, received_msgs);
+    sub.shutdown();
+    process_pending_events();
+    ASSERT_EQ(1, connected_cbs);
+    ASSERT_EQ(1, disconnected_cbs);
 }
 
 TEST(fkie_message_filters_ros_integration, Subscriber)
@@ -59,13 +96,11 @@ TEST(fkie_message_filters_ros_integration, Subscriber)
     flt.set_processing_function([&received_msgs](const ros::MessageEvent<const std_msgs::Empty>&) -> bool { ++received_msgs; return true; });
     ASSERT_EQ(0, pub.getNumSubscribers());
     sub.subscribe(nh, "subscriber_test", 1);
-    ros::spinOnce();
+    process_pending_events();
     ASSERT_EQ(1, pub.getNumSubscribers());
     ASSERT_EQ(0, received_msgs);
     pub.publish<std_msgs::Empty>(std_msgs::Empty());
-    ros::spinOnce();
-    usleep(100000);
-    ros::spinOnce();
+    process_pending_events();
     ASSERT_EQ(1, received_msgs);
 }
 
